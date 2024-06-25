fdbc91a25c488ec51784286053a1e386d9f7e328
Kamal Parashar <kamalparasharp1123@gmail.com>
2024-06-25 21:50:08 +0530

    utils

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+const asyncHandler = (requestHandler) => {
+    (req,res,next) => {
+    Promise.resolve(requestHandler(req, res, next)).
+    catch((error) => next(err))
+}}
+
+export {asyncHandler}
+
+// const asyncHandler = () => {} // function
+// const asyncHandler = (fn) => { () => {} }    // arrow function inside a callback function -- HOF(accepting fn as an argument and/or returning a func)
+// const asyncHandler = (fn) => { async () => {} }  making it async function
+// const asyncHandler = (fn) => async () => {}  // removed curly braces to avoid return statement 
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {  //HOFs - Higher Order Functions
+//     try{
+//         await fn(req, res, next);
+//     }
+//     catch(error){
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
